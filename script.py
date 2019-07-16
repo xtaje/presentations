1d2227dd2f8319dc7d4d077ccf88aff17f509a5b
Ed Jung <edwin.jung@outlook.com>
2019-07-15 21:14:47 -0700

    added credentials, yield bad keys

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ STOCK_SYMBOLS=["MMM", "GOOG", "NFLX"] # and so on ...
 THRESHOLD = 0.5
 
 def find_bad_articles(bucket_name, batch_size=100):
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource('s3', config.aws_key, config.aws_secret)
     objects = s3.Bucket(bucket_name).objects
     for obj in objects.page_size(batch_size):
         buf = cStringIO.StringIO()
@@ -20,4 +20,4 @@ def find_bad_articles(bucket_name, batch_size=100):
                limit -=1 
 
             if limit <= 0:
-                print("Bad file detected %s", obj.key)
+                yield obj.key
