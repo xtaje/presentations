c54259d449d65a575879cbedcea84a190b8578b3
Ed Jung <edwin.jung@outlook.com>
2019-07-15 23:01:06 -0700

    even more functional style

--- script.py
+++ script.py
@@ -2,27 +2,30 @@ from __future__ import print_function
 import config
 import boto3
 import cStringIO
+from itertools import chain
 
 STOCK_SYMBOLS=["MMM", "GOOG", "NFLX"] # and so on ...
 THRESHOLD = 0.5
 
-def find_bad_articles(bucket_name, batch_size): 
-    return _find_bad_articles(fetch_objects(bucket_name, batch_size, s3))
+def find_bad_articles(bucket_name, batch_size, aws_key, aws_secret): 
+    objects = s3.resource(aws_key, aws_secret)
+                .bucket(bucket_name)
+                .objects
+                .page_size(batch_size)
+    return map(is_blacklisted, fetch_objects(objects))
 
-def _find_bad_articles(fetched_objects):
-    for key, text in fetched_objects:
-        words = text.split()
-        limit = THRESHOLD * len(words)
-        for w in words:
-            if w in STOCK_SYMBOLS:
-               limit -=1 
+def is_blacklisted(key, text):
+    words = text.split()
+    limit = THRESHOLD * len(words)
+    for w in words:
+        if w in STOCK_SYMBOLS:
+           limit -=1 
 
-            if limit <= 0:
-                yield key
+        if limit <= 0:
+            yield key
 
-def fetch_objects(bucket_name, batch_size, s3):
-    objects = s3.Bucket(bucket_name).objects
-    for obj in objects.page_size(batch_size)
+def fetch_objects(objects, batch_size, s3):
+    for obj in objects:
         buf = cStringIO.StringIO()
         obj.downloadfileobj(buf)
         yield obj.key, buf.getvalue()
