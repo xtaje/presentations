96300a21eea1b5bf5f1e7b96b16a75a74cf204c3
Ed Jung <edwin.jung@outlook.com>
2019-07-15 21:29:39 -0700

    test using moto instead of patch

--- test_script.py
+++ test_script.py
@@ -1,16 +1,25 @@
 import unittest
-import mock
-
+from moto import mock_s3
 
 
 class TestBadArticleFinder(unittest.TestCase):
-    @mock.patch("script.boto3", autospec=True)
-    def test_find_bad_articles(self, mock_boto3):
-        mock_boto3.resource.Bucket.objects.page_size.return_value = # ?. @$!!?! I give up!
+    @mock_s3
+    def test_find_bad_articles(self):
+        conn = boto3.resource('s3', region_name='us-east-1')
+        conn.create_bucket(Bucket="my_bucket")
 
-        bad_articles = list(find_bad_articles("my_bucket"))
+        bucket_name = "my_bucket"
 
-        # assert something here
+        conn.put_object(Bucket=bucket_name, Key="good_article", 
+            Body="Prince Archie and the Queen ...")
+
+        conn.put_object(Bucket=bucket_name, Key="bad_article", 
+            Body=TSLA -13.0% AAPL 0.22% XOM -1.01% GOOG -2.0% V 0.9% TSLA"
+
+        bad_articles = list(find_bad_articles(bucket_name))
+        self.assertEqual(1, len(bad_articles))
+        self.assert("bad_article", bad_articles[0])
+
 
 
 
