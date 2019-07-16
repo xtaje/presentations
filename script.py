3af1f0a7bfe802cf3c5cba905e0a9d2c503120dd
Ed Jung <edwin.jung@outlook.com>
2019-07-15 22:17:26 -0700

    continuation passing predicate hole-in-the-middle

--- script.py
+++ script.py
@@ -6,18 +6,25 @@ import cStringIO
 STOCK_SYMBOLS=["MMM", "GOOG", "NFLX"] # and so on ...
 THRESHOLD = 0.5
 
-def find_bad_articles(bucket_name, batch_size=100):
+def find_bad_articles(bucket_name, predicate, batch_size=100):
     s3 = boto3.resource('s3', config.aws_key, config.aws_secret)
     objects = s3.Bucket(bucket_name).objects
     for obj in objects.page_size(batch_size):
         buf = cStringIO.StringIO()
         obj.downloadfileobj(buf)
-        words = buf.getvalue().split()
 
-        limit = THRESHOLD * len(words)
-        for w in words:
-            if w in STOCK_SYMBOLS:
-               limit -=1 
+        if predicate(buf.get_value()):
+            yield key
 
-            if limit <= 0:
-                yield obj.key
+
+
+def is_blacklisted(text, threshold=0.5):
+    words = text.split()
+    limit = threshold * len(words)
+    for w in words:
+        if w in STOCK_SYMBOLS:
+           limit -=1 
+
+        if limit <= 0:
+            return True
+    return False
