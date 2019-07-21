96bb30f35ad677539f7fced86d9b96aaa963f5c2
Ed Jung <ejung@quid.com>
2019-07-21 18:51:51 -0500

    pass all parameters in

--- script.py
+++ script.py
@@ -4,17 +4,14 @@ import io
 with open("stock_symbols.txt") as fobj:
     STOCK_SYMBOLS=[line.strip() for line in fobj.readlines()]
 
-REGION_NAME="us-east-2"
-THRESHOLD = 0.5
-PREFIX="news"
 
-def find_bad_articles(bucket_name):
-    client = boto3.client('s3', region_name=REGION_NAME)
+def find_bad_articles(bucket_name, prefix, threshold, region_name):
+    client = boto3.client('s3', region_name=region_name)
 
-    for key in get_keys(client, bucket_name):
+    for key in get_keys(client, bucket_name, prefix):
         lines = download_key(client, bucket_name, key)
 
-        limit = THRESHOLD * len(lines)
+        limit = threshold * len(lines)
         while lines and limit:
             line = lines.pop()
             first_word = line.split()[0]
@@ -24,19 +21,19 @@ def find_bad_articles(bucket_name):
         if limit <= 0:
             yield key
 
-def get_keys(client, bucket_name):
-    pages = get_pages(client, bucket_name)
+def get_keys(client, bucket_name, prefix):
+    pages = get_pages(client, bucket_name, prefix)
     for page in pages:
         for item in page['Contents']:
             key = item['Key']
-            if key == f"{PREFIX}/": #ignore root
+            if key == f"{prefix}/": #ignore root
                 continue
             else:
                 yield key
 
-def get_pages(client, bucket_name):
+def get_pages(client, bucket_name, prefix):
     paginator = client.get_paginator('list_objects')
-    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=PREFIX)
+    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
     return page_iterator
 
 def download_key(client, bucket_name, key):
@@ -49,4 +46,4 @@ def download_key(client, bucket_name, key):
     return lines
 
 if __name__ == "__main__":
-    print(list(find_bad_articles("pybay2019")))
+    print(list(find_bad_articles("pybay2019", "news", 0.5, "us-east-2")))
