5f4d66e558f9850b7931dad5a61e6e75f32450f1
Ed Jung <ejung@quid.com>
2019-07-21 20:13:51 -0500

    dependency injection with ArticleFetcher

---
+++ fetcher.py
@@ -0,0 +1,31 @@
+import io
+
+class ArticleFetcher(object):
+    def __init__(self, client, bucket_name, prefix):
+        self._client = client
+        self._bucket_name = bucket_name
+        self._prefix = prefix
+
+    def get_keys(self):
+        pages = self.get_pages()
+        for page in pages:
+            for item in page['Contents']:
+                key = item['Key']
+                if key == f"{self._prefix}/": #ignore root
+                    continue
+                else:
+                    yield key
+    
+    def get_pages(self):
+        paginator = self._client.get_paginator('list_objects')
+        page_iterator = paginator.paginate(Bucket=self._bucket_name, Prefix=self._prefix)
+        return page_iterator
+    
+    def download_key(self, key):
+        buf = io.BytesIO()
+        self._client.download_fileobj(self._bucket_name, key, buf)
+        buf.seek(0)
+        
+        lines = (line.decode().strip() for line in buf.readlines())
+        lines = list(filter(lambda x:x, lines))
+        return lines
--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import boto3
 import io
+from fetcher import ArticleFetcher
 
 with open("stock_symbols.txt") as fobj:
     STOCK_SYMBOLS=[line.strip() for line in fobj.readlines()]
@@ -8,11 +9,9 @@ REGION_NAME="us-east-2"
 THRESHOLD = 0.5
 PREFIX="news"
 
-def find_bad_articles(bucket_name):
-    client = boto3.client('s3', region_name=REGION_NAME)
-
-    for key in get_keys(client, bucket_name):
-        lines = download_key(client, bucket_name, key)
+def find_bad_articles(fetcher):
+    for key in fetcher.get_keys():
+        lines = fetcher.download_key(key)
 
         limit = THRESHOLD * len(lines)
         while lines and limit:
@@ -24,29 +23,7 @@ def find_bad_articles(bucket_name):
         if limit <= 0:
             yield key
 
-def get_keys(client, bucket_name):
-    pages = get_pages(client, bucket_name)
-    for page in pages:
-        for item in page['Contents']:
-            key = item['Key']
-            if key == f"{PREFIX}/": #ignore root
-                continue
-            else:
-                yield key
-
-def get_pages(client, bucket_name):
-    paginator = client.get_paginator('list_objects')
-    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=PREFIX)
-    return page_iterator
-
-def download_key(client, bucket_name, key):
-    buf = io.BytesIO()
-    client.download_fileobj(bucket_name, key, buf)
-    buf.seek(0)
-    
-    lines = (line.decode().strip() for line in buf.readlines())
-    lines = list(filter(lambda x:x, lines))
-    return lines
-
 if __name__ == "__main__":
-    print(list(find_bad_articles("pybay2019")))
+    client = boto3.client('s3', region_name=REGION_NAME)
+    fetcher = ArticleFetcher(client, "pybay2019", "news")
+    print(list(find_bad_articles(fetcher)))
