891c97ce0a7e005e1330ec4c27cb1dcf80c8ec7f
Ed Jung <ejung@quid.com>
2019-08-04 14:00:18 -0700

    inject the policy

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ class BadStockFeedPolicy(object):
 
         return limit <= 0
 
-def find_bad_articles(bucket_name):
+def find_bad_articles(bucket_name, policy):
     client = boto3.client('s3', region_name=config.REGION_NAME)
     paginator = client.get_paginator('list_objects')
     page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=config.PREFIX)
@@ -35,10 +35,10 @@ def find_bad_articles(bucket_name):
             buf.seek(0)
             lines = (line.decode().strip() for line in buf.readlines())
 
-            policy = BadStockFeedPolicy(config.THRESHOLD, config.stock_symbols())
             if policy.detect_bad_article(lines):
                 yield key
 
 
 if __name__ == "__main__":
-    print(list(find_bad_articles("pybay2019")))
+    policy = BadStockFeedPolicy(config.THRESHOLD, config.stock_symbols())
+    print(list(find_bad_articles("pybay2019", policy)))
