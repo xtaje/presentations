a364907a802bd29c66caf43eab2b8e1bad86acd0
Ed Jung <ejung@quid.com>
2019-08-04 13:50:44 -0700

    Encapsulate responsibility in BadArticlePolicy

--- script.py
+++ script.py
@@ -2,6 +2,23 @@ import boto3
 import io
 import config
 
+
+class BadStockFeedPolicy(object):
+    def __init__(self, threshold, stock_symbols):
+        self._threshold = threshold
+        self._stock_symbols = stock_symbols
+
+    def detect_bad_article(self, lines):
+        lines = list(filter(lambda x:x, lines))
+        limit = self._threshold * len(lines)
+        while lines and limit:
+            line = lines.pop()
+            first_word = line.split()[0]
+            if first_word in self._stock_symbols:
+                limit -= 1
+
+        return limit <= 0
+
 def find_bad_articles(bucket_name):
     client = boto3.client('s3', region_name=config.REGION_NAME)
     paginator = client.get_paginator('list_objects')
@@ -15,19 +32,11 @@ def find_bad_articles(bucket_name):
             buf = io.BytesIO()
             client.download_fileobj(bucket_name, key, buf)
 
-            # Decode and drop empty lines
             buf.seek(0)
             lines = (line.decode().strip() for line in buf.readlines())
-            lines = list(filter(lambda x:x, lines))
 
-            limit = config.THRESHOLD * len(lines)
-            while lines and limit:
-                line = lines.pop()
-                first_word = line.split()[0]
-                if first_word in config.stock_symbols():
-                    limit -= 1
-
-            if limit <= 0:
+            policy = BadStockFeedPolicy(config.THRESHOLD, config.stock_symbols())
+            if policy.detect_bad_article(lines):
                 yield key
 
 
