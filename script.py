8fd84056e33d78add491e0a9af250aac93b4fa9c
Ed Jung <ejung@quid.com>
2019-08-04 14:05:51 -0700

    closure instead of policy class

--- script.py
+++ script.py
@@ -3,23 +3,18 @@ import io
 import config
 
 
-class BadStockFeedPolicy(object):
-    def __init__(self, threshold, stock_symbols):
-        self._threshold = threshold
-        self._stock_symbols = stock_symbols
+def detect_bad_article(lines, threshold, stock_symbols):
+    lines = list(filter(lambda x:x, lines))
+    limit = threshold * len(lines)
+    while lines and limit:
+        line = lines.pop()
+        first_word = line.split()[0]
+        if first_word in stock_symbols:
+            limit -= 1
 
-    def detect_bad_article(self, lines):
-        lines = list(filter(lambda x:x, lines))
-        limit = self._threshold * len(lines)
-        while lines and limit:
-            line = lines.pop()
-            first_word = line.split()[0]
-            if first_word in self._stock_symbols:
-                limit -= 1
+    return limit <= 0
 
-        return limit <= 0
-
-def find_bad_articles(bucket_name, policy):
+def find_bad_articles(bucket_name, is_bad_article):
     client = boto3.client('s3', region_name=config.REGION_NAME)
     paginator = client.get_paginator('list_objects')
     page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=config.PREFIX)
@@ -35,10 +30,14 @@ def find_bad_articles(bucket_name, policy):
             buf.seek(0)
             lines = (line.decode().strip() for line in buf.readlines())
 
-            if policy.detect_bad_article(lines):
+            if is_bad_article(lines):
                 yield key
 
 
 if __name__ == "__main__":
-    policy = BadStockFeedPolicy(config.THRESHOLD, config.stock_symbols())
-    print(list(find_bad_articles("pybay2019", policy)))
+    def is_bad_article(lines):
+        return detect_bad_article(lines, 
+                threshold=config.THRESHOLD,
+                stock_symbols=config.stock_symbols()
+                )
+    print(list(find_bad_articles("pybay2019", is_bad_article)))
