dbf6bf63741529f3b65a37d472772d83f85e72c5
Ed Jung <ejung@quid.com>
2019-08-06 21:41:56 -0700

    more refinement

--- script.py
+++ script.py
@@ -12,7 +12,9 @@ def find_bad_articles(article_source, predicate):
 
 if __name__ == "__main__":
     client = boto3.client('s3', region_name=config.REGION_NAME)
-    print(list(find_bad_articles(S3Fetcher(client, "pybay2019", config.PREFIX)))
+    policy = StockArticlePolicy(config.threshold, config.get_stock_symbols())
+    article_source = S3Fetcher(client, "pybay2019", "config.PREFIX")
+    print(list(find_bad_articles(article_source, policy))
 
 
 
