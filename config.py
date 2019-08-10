9dd76dc6ca3d7904ade80ff21ad9ffdf7f665a90
Ed Jung <ejung@quid.com>
2019-08-10 11:42:27 -0700

    config.STOCK_SYMBOLS

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-_STOCK_SYMBOLS=None
-
-def stock_symbols():
-    global _STOCK_SYMBOLS
-    if _STOCK_SYMBOLS is None:
-        with open("stock_symbols.txt") as fobj:
-            _STOCK_SYMBOLS=[line.strip() for line in fobj.readlines()]
-    return _STOCK_SYMBOLS
+STOCK_SYMBOLS=None
+if STOCK_SYMBOLS is None:
+    with open("stock_symbols.txt") as fobj:
+        STOCK_SYMBOLS=[line.strip() for line in fobj.readlines()]
 
 REGION_NAME="us-east-2"
 THRESHOLD = 0.5
