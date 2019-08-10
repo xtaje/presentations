17ff0aedbdfdbdc843734d9ae003cad26cdc18c1
Ed Jung <ejung@quid.com>
2019-08-10 12:50:00 -0700

    inject archive, predicate

--- script.py
+++ script.py
@@ -4,15 +4,19 @@ import config
 from util import check_article
 from s3 import S3Archive
 
-def find_bad_articles(bucket_name):
-    archive = S3Archive(bucket_name, 
-                        config.PREFIX,
-                        config.REGION_NAME)
+def find_bad_articles(archive, predicate):
     for key in archive.get_keys():
         lines = archive.get_file(key)
-        if check_article(lines, config.THRESHOLD, config.STOCK_SYMBOLS):
+        if predicate(lines):
             yield key
 
 
 if __name__ == "__main__":
-    print(list(find_bad_articles("pybay2019")))
+    def is_stock_quotes(lines):
+        return check_article(lines, config.THRESHOLD, config.STOCK_SYMBOLS)
+
+    archive = S3Archive("pybay2019", 
+                        config.PREFIX,
+                        config.REGION_NAME)
+
+    print(list(find_bad_articles(archive, is_stock_quotes)))
