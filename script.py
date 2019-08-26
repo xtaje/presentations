f0ed8484d89d022d9841831cad9fa71943b3690a
Ed Jung <ejung@quid.com>
2019-08-25 17:50:06 -0700

    closure as predciate; (tests broken)

--- script.py
+++ script.py
@@ -1,4 +1,8 @@
 from newscheck.core import find_bad_articles 
+from newscheck.util import check_article
+from newscheck import config, stock
 
 if __name__ == "__main__":
-    print(list(find_bad_articles("pybay2019")))
+    def is_stock_quotes(lines):
+        return check_article(lines, config.THRESHOLD, stock.TICKER_SYMBOLS)
+    print(list(find_bad_articles("pybay2019", is_stock_quotes)))
