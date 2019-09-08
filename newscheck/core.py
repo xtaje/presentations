90ac3f6256844205a7dd7800b3d866bbb7316c02
Ed Jung <ejung@quid.com>
2019-09-08 15:37:23 -0700

    cleaner patchy test

--- newscheck/core.py
+++ newscheck/core.py
@@ -8,7 +8,7 @@ from newscheck.util import get_pages, keys_from, get_file, check_article
 def find_bad_articles(bucket_name):
     client = boto3.client('s3', region_name=config.REGION_NAME)
 
-    for page in get_pages(client, bucket_name, config.PREFIX):
+    for page in get_pages(client, bucket_name=bucket_name, prefix=config.PREFIX):
         for key in keys_from(page):
             lines = get_file(client, bucket_name, key)
             if check_article(lines, config.THRESHOLD, stock.TICKER_SYMBOLS):
--- newscheck/test_script.py
+++ newscheck/test_script.py
@@ -1,22 +1,17 @@
 import unittest
-from unittest import mock
-from mock import MagicMock, call
+from unittest.mock import MagicMock, call, patch
 from newscheck.core import find_bad_articles
 
 class TestBadArticleFinder(unittest.TestCase):
-    @mock.patch("newscheck.core.get_file")
-    @mock.patch("newscheck.core.get_pages")
-    @mock.patch("newscheck.core.boto3.client")
+    @patch("newscheck.core.get_file")
+    @patch("newscheck.core.get_pages")
+    @patch("newscheck.core.boto3.client")
     def test_find_bad_articles_mocked(self, client_mock, get_pages_mock, get_file_mock):
-        mock_client = MagicMock(name="mock s3 client")
-        client_mock.return_value = mock_client
-        bucket_name = "my_bucket"
         page_stub = [{
                 "Contents": [
-                    { "Key": 'news/'},
-                    { "Key": 'news/good.txt'},
-                    { "Key": 'news/bad.txt'}
-                    ]
+                    {"Key": 'news/'},
+                    {"Key": 'news/good.txt'},
+                    {"Key": 'news/bad.txt'}]
         }]
         get_pages_mock.return_value = page_stub
 
@@ -26,20 +21,17 @@ class TestBadArticleFinder(unittest.TestCase):
         ]
         get_file_mock.side_effect = file_stubs 
 
-        bad_articles = list(find_bad_articles(bucket_name))
-        self.assertEqual(1, len(bad_articles))
-        self.assertEqual("news/bad.txt", bad_articles[0])
-
-        self.assertEqual([call(mock_client, "my_bucket", "news")], 
-                          get_pages_mock.mock_calls)
+        bad_articles = list(find_bad_articles("my_bucket"))
+        self.assertEqual(["news/bad.txt"], bad_articles)
 
         self.assertEqual([
-                call(mock_client,"my_bucket", "news/good.txt"),
-                call(mock_client,"my_bucket", "news/bad.txt")
+                call(client_mock.return_value, "my_bucket", "news/good.txt"),
+                call(client_mock.return_value, "my_bucket", "news/bad.txt")
             ],
             get_file_mock.mock_calls
         )
 
+    @unittest.skip("for manual use")
     def test_find_bad_articles(self):
         bucket_name = "pybay2019"
 
